77de24c58666de0941ed169279232e61d88c3cd9
Antoine Grondin <antoinegrondin@gmail.com>
2014-02-25 23:11:28 -0500

    Fix log level brain fart.

--- logger/logger.go
+++ logger/logger.go
@@ -24,9 +24,6 @@ type logger struct {
 
 func New(prefix, filename string, lvl Level) *logger {
 	fd, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	// if os.IsNotExist(err) {
-	// 	fd, err = os.Create(filename)
-	// }
 	if err != nil {
 		panic(fmt.Sprintf("creating log file '%s', %v", filename, err))
 	}
@@ -37,40 +34,40 @@ func New(prefix, filename string, lvl Level) *logger {
 }
 
 func (l *logger) Debugf(msg string, arg ...interface{}) {
-	if l.lvl > Debug {
+	if l.lvl < Debug {
 		return
 	}
 	l.l.Printf("[Debug] "+msg, arg...)
 }
 
 func (l *logger) Infof(msg string, arg ...interface{}) {
-	if l.lvl > Info {
+	if l.lvl < Info {
 		return
 	}
 	l.l.Printf("[Info] "+msg, arg...)
 }
 func (l *logger) Warnf(msg string, arg ...interface{}) {
-	if l.lvl > Warn {
+	if l.lvl < Warn {
 		return
 	}
 	l.l.Printf("[Warn] "+msg, arg...)
 }
 func (l *logger) Errorf(msg string, arg ...interface{}) {
-	if l.lvl > Error {
+	if l.lvl < Error {
 		return
 	}
 	l.l.Printf("[Error] "+msg, arg...)
 }
 
 func (l *logger) Fatalf(msg string, arg ...interface{}) {
-	if l.lvl > Fatal {
+	if l.lvl < Fatal {
 		return
 	}
 	l.l.Fatalf("[Fatal] "+msg, arg...)
 }
 
 func (l *logger) Panicf(msg string, arg ...interface{}) {
-	if l.lvl > Panic {
+	if l.lvl < Panic {
 		return
 	}
 	l.l.Panicf("[Panic] "+msg, arg...)
