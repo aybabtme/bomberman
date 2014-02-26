5a5564dfce1aa10ed76b2a260f35106b09726369
Antoine Grondin <antoinegrondin@gmail.com>
2014-02-25 21:21:00 -0500

    Test to use when Decode/Encode will be solved for scheduler.
    Will be required for multiplayer support/saved games.

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -42,3 +42,50 @@ func ExampleScheduler_simple() {
 	// Bye
 	// Still there?
 }
+
+// func ExampleScheduler_serialized() {
+// 	s := scheduler.NewScheduler()
+
+// 	actions := []struct {
+// 		action PrintAction
+// 		time   int
+// 	}{
+// 		{PrintAction("Still there?"), 5},
+// 		{PrintAction("Hello"), 1},
+// 		{PrintAction("Bye"), 3},
+// 		{PrintAction("Bonjour"), 2},
+// 		{PrintAction("Bye"), 3},
+// 	}
+
+// 	for _, action := range actions {
+// 		s.Register(action.action, action.time)
+// 	}
+
+// 	buf := bytes.NewBuffer(nil)
+// 	err := s.Encode(buf)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	log.Printf("Scheduler=%s", buf.String())
+
+// 	back, err := scheduler.Decode(buf)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	for back.HasNext() {
+// 		back.NextTurn()
+// 		back.DoTurn(func(a scheduler.Action, turn int) error {
+// 			fmt.Println(a.(PrintAction))
+// 			return nil
+// 		})
+// 	}
+
+// 	// Output:
+// 	// Hello
+// 	// Bonjour
+// 	// Bye
+// 	// Bye
+// 	// Still there?
+// }
